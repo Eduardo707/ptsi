ea21896e1796cadfa3e5698684a01ed1c3b3fb04
charlie_vo <luisv.moliveira@gmail.com>
2018-05-29 09:56:30 +0000

    das

--- server.js
+++ server.js
@@ -37,8 +37,9 @@ var moment= require('moment');
 
 
 
+router.use(express.static(__dirname, '/views'));
 
-router.use(express.static(path.resolve(__dirname, 'views')));
+//router.use(express.static(path.resolve(__dirname, 'views')));
 var messages = [];
 var sockets = [];
 
