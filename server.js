6188282e26ddca713f6815e785d85c7b731907d5
charlie_vo <luisv.moliveira@gmail.com>
2018-05-29 09:35:06 +0000

    suu

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 //
 // A simple chat server using Socket.IO, Express, and Async.
 //
-var http = require('http');
+/*var http = require('http');
 var path = require('path');
 
 var async = require('async');
@@ -85,3 +85,4 @@ server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
+*/
