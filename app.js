49a0eec3d24ef4aea866bf0bc5f664a252b170f5
charlie_vo <luisv.moliveira@gmail.com>
2018-06-06 14:11:06 +0000

    maybe1.4

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ var LocalStrategy= require("passport-local");
 var flash= require('connect-flash');
 var cookieParser= require('cookie-parser');
 var session= require("express-session");
-var moment= require('moment');
 
 
 app.use(express.static(path.resolve(__dirname, 'client')));
