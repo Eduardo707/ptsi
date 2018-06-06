816abd406a1babf6324a976626bcdb5de79d0ec1
charlie_vo <luisv.moliveira@gmail.com>
2018-06-06 13:54:01 +0000

    maybe1.3

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ var LocalStrategy= require("passport-local");
 
 var flash= require('connect-flash');
 
+app.use(express.static(path.resolve(__dirname, 'client')));
+
 /*app.listen(process.env.PORT || 5000, process.env.IP || "0.0.0.0", function(){
   var addr = app.address();
   console.log("Chat server listening at", addr.address + ":" + addr.port);
