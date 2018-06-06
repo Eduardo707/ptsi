76246f7ce2779dccdc8b1e50439c45627879d52d
charlie_vo <luisv.moliveira@gmail.com>
2018-06-06 13:56:51 +0000

    maybe1.3

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,5 +6,9 @@ var router =  new express();
 var routes= require('./allRoutes');
 //app.use('/', index);
 router.use('/', routes);
+router.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = router.address();
+  console.log("Chat server listening at", addr.address + ":" + addr.port);
+});
 
 module.exports = router;
