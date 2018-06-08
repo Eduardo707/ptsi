6ea5956b995e0f96cc7f45755427015972cc87df
charlie_vo <luisv.moliveira@gmail.com>
2018-06-08 16:32:12 +0000

    vcs

--- api/routes/allRoutes.js
+++ api/routes/allRoutes.js
@@ -45,7 +45,7 @@ function loggedIn(req, res, next) {
 
  router.get('/logout',log.logout);
   router.post('/login',passport.authenticate("local"), function (req, res) {
-   res.json('success');
+   res.json(req.user);
 });
   
   
