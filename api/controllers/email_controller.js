29b005b18f16ffdbbc808f8f3d942864d11dcc2e
charlie_vo <luisv.moliveira@gmail.com>
2019-02-28 15:10:47 +0000

    2019-2

--- api/controllers/email_controller.js
+++ api/controllers/email_controller.js
@@ -6,7 +6,7 @@ var passport = require("passport");
 var async = require("async");
 var crypto = require("crypto");
 var nodemailer = require("nodemailer");
- var dotenv = require('dotenv').config();
+require('dotenv').config();
 
 
 
