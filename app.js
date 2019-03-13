9a00122980f1202ea57e2a7cf5a53992e82a47bc
charlie_vo <luisv.moliveira@gmail.com>
2019-03-13 20:30:07 +0000

    chat

--- app.js
+++ app.js
@@ -279,8 +279,10 @@ function broadcast(event, data) {
 
 
 
+app.listen( 3000);
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+
+server.listen(process.env.PORT || 5000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
