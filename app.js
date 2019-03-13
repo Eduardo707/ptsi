e621cb28c290731ba11467b548dbca927700e773
charlie_vo <luisv.moliveira@gmail.com>
2019-03-13 22:03:18 +0000

    chat

--- app.js
+++ app.js
@@ -275,7 +275,7 @@ module.exports= app;
 
 var port = process.env.PORT || 3000;
 //app.listen(5000);
-server.listen(port, function () {
+app.listen(port, function () {
  console.log(`Example app listening on port !`);
 });
 //server.listen(app);
