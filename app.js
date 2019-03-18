bec7a3dcb3d1af0a4e9b1b22ba422249e80be1ce
charlie_vo <luisv.moliveira@gmail.com>
2019-03-18 16:41:34 +0000

    ffffffff

--- app.js
+++ app.js
@@ -37,13 +37,7 @@ server.listen(process.env.PORT || 3000,  function(){
 });*/
 app.use(express.static(path.resolve(__dirname, 'views')));
 
- app.use(function(req, res, next){
-     res.header("Access-Control-Allow-Origin", "*");
-     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-     res.header('Access-Control-Allow-Credentials', "true");
-     next();
- });
+
 /*var methodOverride= require("method-override");
 app.use(methodOverride("_method"));*/
 //app.use(express.static(path.resolve(__dirname, 'views')));
