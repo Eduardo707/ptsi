2e90f7713fd658e52652eb0eec79f89cb27f11d3
charlie_vo <luisv.moliveira@gmail.com>
2019-03-18 16:53:39 +0000

    ffffffff

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ var session = require("express-session");
 var LocalStrategy= require("passport-local").Strategy;
 var BearerStrategy= require("passport-http-bearer").Strategy;
  
+  app.use(function(req, res, next){
+     res.header("Access-Control-Allow-Origin", "*");
+     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+     res.header('Access-Control-Allow-Credentials', "true");
+     next();
+ });
      
 var http = require('http');
 
