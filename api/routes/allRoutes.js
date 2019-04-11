5b90ffbcca85ef734a8693e86a445462c92adb09
Eduardo Ferreira <eduardomf7@hotmail.com>
2019-04-11 20:56:31 +0000

    new

--- api/routes/allRoutes.js
+++ api/routes/allRoutes.js
@@ -101,7 +101,7 @@ res.status(200).json({msg:"true",  user: req.user});}
            
         }
         else{
-res.status(200).json({msg:"true",  user: req.user});}
+res.status(200).json({msg:"true", user: req.user});}
 });
     //});
     
