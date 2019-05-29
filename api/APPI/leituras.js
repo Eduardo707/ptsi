85a64931a73b530d3beacb5f7ffd5142ff545170
Eduardo Ferreira <eduardomf7@hotmail.com>
2019-05-29 16:58:13 +0000

    unique

--- api/APPI/leituras.js
+++ api/APPI/leituras.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var leiturasSchema = new mongoose.Schema({
 
 
-    patientID: {type: Date, unique: false, required: true},
+    patientID: {type: String, unique: false, required: true},
         data_resg: {type: Date, unique: true, required: true},
 
     type: String,  
